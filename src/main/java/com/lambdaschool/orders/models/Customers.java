5e10368edae372d3a40c86cdf4f3175cc298ccd1
ren.meta@gmail.com <ren.meta@gmail.com>
2019-09-11 17:20:39 -0400

    added @OneToMany injection to customers class

--- src/main/java/com/lambdaschool/orders/models/Customers.java
+++ src/main/java/com/lambdaschool/orders/models/Customers.java
@@ -3,6 +3,8 @@ package com.lambdaschool.orders.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "customers")
@@ -30,6 +32,12 @@ public class Customers
                 nullable = false)
     @JsonIgnoreProperties("customers")
     private Agents agents;
+
+    @OneToMany(mappedBy = "customers",
+                cascade = CascadeType.ALL,
+                orphanRemoval = false)
+    @JsonIgnoreProperties("customers")
+    private List<Orders> orders = new ArrayList<>();
 
     public Customers()
     {
