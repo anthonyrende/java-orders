538fe42c0489080cc43365489a9386d02eb7e74c
ren.meta@gmail.com <ren.meta@gmail.com>
2019-09-11 17:17:35 -0400

    customers class finished

--- src/main/java/com/lambdaschool/orders/models/Customers.java
+++ src/main/java/com/lambdaschool/orders/models/Customers.java
@@ -1,6 +1,160 @@
 package com.lambdaschool.orders.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
 public class Customers
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long custcode;
 
+    @Column(unique = true,
+            nullable = false)
+    private String custname;
+
+    private String custcity;
+    private String workingarea;
+    private String custcountry;
+    private String grade;
+    private double openingamt;
+    private double receiveamt;
+    private double outstandingamt;
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "agentcode",
+                nullable = false)
+    @JsonIgnoreProperties("customers")
+    private Agents agents;
+
+    public Customers()
+    {
+    }
+
+    public Customers(String custname, String custcity, String workingarea, String custcountry, String grade, double openingamt, double receiveamt, double outstandingamt, String phone, Agents agents)
+    {
+        this.custname = custname;
+        this.custcity = custcity;
+        this.workingarea = workingarea;
+        this.custcountry = custcountry;
+        this.grade = grade;
+        this.openingamt = openingamt;
+        this.receiveamt = receiveamt;
+        this.outstandingamt = outstandingamt;
+        this.phone = phone;
+        this.agents = agents;
+    }
+
+    public long getCustcode()
+    {
+        return custcode;
+    }
+
+    public void setCustcode(long custcode)
+    {
+        this.custcode = custcode;
+    }
+
+    public String getCustname() {
+        return custname;
+    }
+
+    public void setCustname(String custname)
+    {
+        this.custname = custname;
+    }
+
+    public String getCustcity()
+    {
+        return custcity;
+    }
+
+    public void setCustcity(String custcity)
+    {
+        this.custcity = custcity;
+    }
+
+    public String getWorkingarea()
+    {
+        return workingarea;
+    }
+
+    public void setWorkingarea(String workingarea)
+    {
+        this.workingarea = workingarea;
+    }
+
+    public String getCustcountry()
+    {
+        return custcountry;
+    }
+
+    public void setCustcountry(String custcountry)
+    {
+        this.custcountry = custcountry;
+    }
+
+    public String getGrade()
+    {
+        return grade;
+    }
+
+    public void setGrade(String grade)
+    {
+        this.grade = grade;
+    }
+
+    public double getOpeningamt()
+    {
+        return openingamt;
+    }
+
+    public void setOpeningamt(double openingamt)
+    {
+        this.openingamt = openingamt;
+    }
+
+    public double getReceiveamt()
+    {
+        return receiveamt;
+    }
+
+    public void setReceiveamt(double receiveamt)
+    {
+        this.receiveamt = receiveamt;
+    }
+
+    public double getOutstandingamt()
+    {
+        return outstandingamt;
+    }
+
+    public void setOutstandingamt(double outstandingamt)
+    {
+        this.outstandingamt = outstandingamt;
+    }
+
+    public String getPhone()
+    {
+        return phone;
+    }
+
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
+
+    public Agents getAgents()
+    {
+        return agents;
+    }
+
+    public void setAgents(Agents agents)
+    {
+        this.agents = agents;
+    }
 }
